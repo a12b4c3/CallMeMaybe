19f5c7d77c998a8c5219d7f5c723da944d736ca9
Alex <k9o8@ugrad.cs.ubc.ca>
2020-06-15 01:41:53 -0700

    demo test

---
+++ src/test/java/tdemo/Question.java
@@ -0,0 +1,48 @@
+package tdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question {
+    List<String> subquestions;
+    public Question() {
+        this.subquestions = new ArrayList<String>();
+    }
+
+    public boolean addedNewQuestion(String question) {
+        int qnumber_old = subquestions.size();
+        if (qnumber_old > 5) {
+            return false;
+        } else {
+            subquestions.add(question);
+            return true;
+        }
+    }
+
+    public void listQuestions() {
+        for (int i = 0; i < subquestions.size(); i++) {
+            String q = this.getQuestion(i);;
+            this.YELLTHEQUESTION(q);
+            System.out.println(q);
+        }
+    }
+
+    public String getQuestion(int i) {
+        return this.subquestions.get(i);
+    }
+
+    public void deleteAllQuestions() {
+        while(this.subquestions.size() != 0) {
+            for (int i = this.subquestions.size()-1; i >= 0 ; i--) {
+                this.subquestions.remove(i);
+            }
+            System.out.println("All questions removed!");
+        }
+    }
+
+    public void YELLTHEQUESTION(String question) {
+        System.out.println("IM YeLlING");
+        question = question.toUpperCase();
+        System.out.println(question);
+    }
+}
